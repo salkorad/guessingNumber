6957c495f41d4c2dd6b699fd1989912f3285187f
salkora <salko.radovovic@gmail.com>
2022-11-01 14:20:05 +0100

    Commit native app

---
+++ components/ui/Card.js
@@ -0,0 +1,25 @@
+import { StyleSheet, View, Dimensions } from 'react-native';
+import Colors from '../../constants/colors';
+
+function Card({ children }) {
+    return (
+        <View style={styles.card}>{children}</View>
+    )
+}
+
+export default Card;
+
+const deviceWidth = Dimensions.get('window').width;
+
+const styles = StyleSheet.create({
+    card: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+        marginTop: deviceWidth < 380 ? 18 : 36,
+        marginHorizontal: 24,
+        borderRadius: 8,
+        backgroundColor: Colors.primary800,
+        elevation: 4,
+    },
+})
